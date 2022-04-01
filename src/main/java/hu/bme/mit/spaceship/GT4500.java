45d1cfeca7d61a04b3592eff6800a089fdaa0015
marczipano <bencze.mark.jeno@gmail.com>
2022-04-01 12:52:39 +0200

    Question in ALL implemetation

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,6 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
+        // IS OK?
         break;
     }
 
